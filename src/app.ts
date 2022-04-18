beeda293297322a956236905f05c7caafac7ae20
Ulrico Carmona <ulricocarmona@gmail.com>
2022-04-18 18:17:54 +0200

    finished finance logger

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,57 @@
-//const form = document.querySelector('form');
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplate} from "./classes/ListTemplate.js";
+
+/*let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice("yoshi", "web work", 250);
+docTwo = new Payment("mario", "plumbing work", 200);
+
+let docs: HasFormatter[] = [];
+
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("Luiggi", "work on the Luiggi website", 300);
+
+//console.log(invOne, invTwo);
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.details, inv.amount, inv.format());
+});*/
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
-//console.log(form.children);
 
-const type = document.querySelector('#type') as HTMLSelectElement
+const type = document.querySelector("#type") as HTMLSelectElement;
 
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 
-const details = document.querySelector('#details') as HTMLInputElement
+const details = document.querySelector("#details") as HTMLInputElement;
 
-const amount = document.querySelector('#amount') as HTMLInputElement
+const amount = document.querySelector("#amount") as HTMLInputElement;
+//list template instance
+const ul = document.querySelector("ul")!;
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
+const list = new ListTemplate(ul);
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    )
-})
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+  };
+  list.render(doc, type.value, 'end');
+  
+});
